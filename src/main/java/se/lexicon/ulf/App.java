6ef7ce6168bc0d689623481a9ca99d540c4045dc
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-09-02 15:02:42 +0200

    init.

---
+++ src/main/java/se/lexicon/ulf/App.java
@@ -0,0 +1,19 @@
+package se.lexicon.ulf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        new App().doSumthing();
+    }
+
+    public void doSumthing()
+    {
+        System.out.println("more text");
+    }
+}
